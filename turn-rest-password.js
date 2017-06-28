584c6f21614f99a58abb0801f32bb5acc85b8d0a
skystar-p <wogus150@naver.com>
2017-06-28 14:40:48 +0900

    config bug fixed

--- turn-rest-password.js
+++ turn-rest-password.js
@@ -4,6 +4,7 @@ let config = {
     turn_port: 3478,
     turns_port: 5349,
     host: '',
+    shared_secret: '',
 };
 
 function generate_username(username) {
@@ -21,15 +22,15 @@ function generate_password(usercombo, shared_secret) {
 function issue_credential() {
     let response = {};
     response.username = generate_username(config.username);
-    response.password = generate_password(response.username);
+    response.password = generate_password(response.username, config.shared_secret);
     response.ttl = config.ttl;
 
     let uris = [];
-    if (turn_port) {
-        uris.push('turn:' + config.host + ':' + turn_port);
+    if (config.turn_port) {
+        uris.push('turn:' + config.host + ':' + config.turn_port);
     }
-    if (turns_port) {
-        uris.push('turns:' + config.host + ':' + turns_port);
+    if (config.turns_port) {
+        uris.push('turns:' + config.host + ':' + config.turns_port);
     }
 
     response.uris = uris;
@@ -38,6 +39,9 @@ function issue_credential() {
 }
 
 function main(_config) {
+    if (_config.username) {
+        config.username = _config.username;
+    }
     if (_config.ttl) {
         config.ttl = _config.ttl;
     }
@@ -53,8 +57,14 @@ function main(_config) {
     else {
         config.host = _config.host;
     }
-    if (!(_config.turn_port) && !(_config.turns_port)) {
+    if (!(config.turn_port) && !(config.turns_port)) {
         throw "at least one turn/turns port should be set.";
+    }
+    if (!_config.shared_secret) {
+        throw "shared_secret value must be set";
+    }
+    else {
+        config.shared_secret = _config.shared_secret;
     }
 
     return issue_credential();
